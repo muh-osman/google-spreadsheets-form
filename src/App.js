b4ec76875fc4db06aa13dec221f6ffe092974a1e
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2023-04-30 23:25:48 +0300

    Fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef } from "react";
+// Bootstrap
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function App() {
+
+  const formEle = useRef();
+  const emailInput = useRef();
+  const passwordInput = useRef();
+
+
+  function submitHandler(e) {
+    e.preventDefault();
+
+
+    const formData = new FormData(formEle.current);
+    fetch(
+      "https://script.google.com/macros/s/AKfycbwPb1u07MSBMYo4bomMIIChuZexI5mMi7gDrq6bhxTHDutUtoROKwEdht2IVXt3PPDa/exec",
+      {
+        method: "POST",
+        body: formData,
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    emailInput.current.value = "";
+    passwordInput.current.value = "";
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Form ref={formEle} noValidate validated={false}>
+        {/* Email */}
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            ref={emailInput}
+            type="email"
+            name="Email"
+            placeholder="Enter email"
+            required
+          />
+        </Form.Group>
+
+        {/* Password */}
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            ref={passwordInput}
+            type="password"
+            name="Password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+
+        {/* Button */}
+        <Button
+          onClick={(e) => {
+            submitHandler(e);
+          }}
+          variant="primary"
+          type="submit"
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
