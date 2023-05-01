59ce646abae639a88e4b998f561df8da28a70263
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2023-05-02 00:15:50 +0300

    Bug Fixes

--- src/App.js
+++ src/App.js
@@ -4,97 +4,114 @@ import { useRef, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
+import InputGroup from "react-bootstrap/InputGroup";
 
 function App() {
   const form = useRef();
   const [alertMsg, setAlertMsg] = useState("");
   const [validated, setValidated] = useState(false);
-  const btn = useRef()
+  const btn = useRef();
 
   let alert = <Alert variant={"warning"}> {alertMsg} </Alert>;
 
   function submitHandler(e) {
     e.preventDefault();
-    btn.current.classList.add('btn_loader')
-    
+    btn.current.classList.add("btn_loader");
+    const formData = new FormData(form.current);
+
     if (form.current.checkValidity() === false) {
-      btn.current.classList.remove('btn_loader')
+      btn.current.classList.remove("btn_loader");
       e.stopPropagation();
       setValidated(true);
-
     } else {
-      const formData = new FormData(form.current);
       fetch(
         "https://script.google.com/macros/s/AKfycbwbZJD06dt4ml-zOUgeG39wtlbZPFh6jmowhJNqXVehP0XBcMtOMMdFlf7VrHZujoaqXA/exec",
         {
           method: "POST",
           body: formData,
         }
-        )
+      )
         .then((response) => {
-          document.querySelector(".flip-card-inner").style.transform = "rotateY(180deg)";
-          setValidated(false);
+          document.querySelector(".flip-card-inner").style.transform =
+            "rotateY(180deg)";
         })
         .catch((error) => {
           setAlertMsg(error.message);
-          btn.current.classList.remove('btn_loader')
+          btn.current.classList.remove("btn_loader");
         });
     }
   }
 
   return (
-    <div className="App flip-card">
-      <div className="flip-card-inner">
-        {alertMsg && alert}
+    <>
+      <h1 className="mt-5 mb-3">Contact Us</h1>
 
-        <Form
-          className="flip-card-front"
-          ref={form}
-          noValidate
-          validated={validated}
-        >
-          {/* Email */}
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              name="email"
-              placeholder="Enter email"
-              required
-            />
-          </Form.Group>
+      <div className="App flip-card">
+        <div className="flip-card-inner">
+          {alertMsg && alert}
 
-          {/* Password */}
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              name="password"
-              placeholder="Password"
-              required
-            />
-          </Form.Group>
-
-          {/* Button */}
-          <Button
-            onClick={(e) => {
-              submitHandler(e);
-            }}
-            variant="primary"
-            type="submit"
-            ref={btn}
+          <Form
+            className="flip-card-front"
+            ref={form}
+            noValidate
+            validated={validated}
           >
-            Submit
-          </Button>
-        </Form>
+            {/* Your name */}
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>Your name</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                placeholder="Enter your name"
+                required
+              />
+            </Form.Group>
 
-        <div className="flip-card-back">
-          <h1>Thank You!</h1>
-          <h3>Your submission has been sent.</h3>
+            {/* Email */}
+            <Form.Label>Email address</Form.Label>
+            <InputGroup className="mb-3">
+              <Form.Control
+                placeholder="Enter email"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+                type="email"
+                name="email"
+                required
+              />
+            </InputGroup>
+
+            {/* Your message */}
+            <Form.Label>Your message</Form.Label>
+            <InputGroup className="mb-3">
+              <Form.Control
+                as="textarea"
+                name="textarea"
+                aria-label="textarea"
+                placeholder="Enter your message"
+                required
+              />
+            </InputGroup>
+
+            {/* Button */}
+            <Button
+              onClick={(e) => {
+                submitHandler(e);
+              }}
+              variant="primary"
+              type="submit"
+              ref={btn}
+            >
+              Submit
+            </Button>
+          </Form>
+
+          <div className="flip-card-back">
+            <h1>Thank You!</h1>
+            <h3>Your submission has been sent.</h3>
+          </div>
         </div>
-
       </div>
-    </div>
+    </>
   );
 }
 
